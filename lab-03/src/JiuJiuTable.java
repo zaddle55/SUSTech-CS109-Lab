a72f27e663a77cfb213dd4f658bf58854e7d4a96
zaddle55 <chenyifan20050115@126.com>
2024-03-07 11:15:17 +0800

    revise the format of table

--- lab-03/src/JiuJiuTable.java
+++ lab-03/src/JiuJiuTable.java
@@ -2,7 +2,7 @@ public class JiuJiuTable {
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.printf("%d*%d=%d ", j, i, i * j);
+                System.out.printf("%d*%d=%-2d ", j, i, i * j);
             }
             System.out.println();
         }
--- lab-03/src/NumberTriangle.java
+++ lab-03/src/NumberTriangle.java
@@ -1,3 +1,4 @@
+
 public class NumberTriangle {
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
